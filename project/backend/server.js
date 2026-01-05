a382904ddad162a2b6ddcc80619bd924f4472569
zainkhokhar1 <muhammadzain7113@gmail.com>
2026-01-06 01:43:24 +0500

    project backend setup

---
+++ project/backend/server.js
@@ -0,0 +1,18 @@
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
