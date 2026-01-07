8f71fb331b4d8d78d3758005b6765bad8a487c0f
zainkhokhar1 <muhammadzain7113@gmail.com>
2026-01-07 12:28:13 +0500

    backend mongodb connection established

---
+++ project/backend/config/db.connect.js
@@ -0,0 +1,17 @@
+// here connecting to the mongodb database
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error.message);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
--- project/backend/server.js
+++ project/backend/server.js
@@ -1,6 +1,7 @@
 
 import express from 'express';
 import cors from 'cors';
+import connectDB from './config/db.connect.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// connect to database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
